3dab7249fd2680d79d53b6ce12c9611c4ad10880
skolesnikovroox <34157996+skolesnikovroox@users.noreply.github.com>
2018-11-28 17:03:15 +0300

    javadoc comment

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,6 +12,10 @@ public class Greeter {
 
   }
 
+  /**
+  * @param param pam
+  * @return of the great white dope
+  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
