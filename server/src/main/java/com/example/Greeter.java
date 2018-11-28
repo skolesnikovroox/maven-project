4b51b5647c2dc9bd7f652dfa36c579c62f8014b5
skolesnikovroox <34157996+skolesnikovroox@users.noreply.github.com>
2018-11-28 17:12:11 +0300

    fix fox comment spaces and tags

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,9 +13,9 @@ public class Greeter {
   }
 
   /**
-  * @param param pam
-  * @return of the great white dope
-  */
+   * @param someone the name of a person
+   * @return greeting string
+   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
